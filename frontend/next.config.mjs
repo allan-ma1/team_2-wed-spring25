0c56bd39ea253c34a2e3933ae6d1317ea17077f1
Allan Ma <allan_ma@outlook.com>
2025-03-14 00:52:59 -0400

    Initial commit

---
+++ frontend/next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    
+    transpilePackages: ['leaflet', 'leaflet-routing-machine'],
+};
+
+export default nextConfig;
